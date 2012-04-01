84c9d1325b681fe441db44a22de1c95100bae2d5
Andrew McFague <redmumba@gmail.com>
2012-04-01 15:41:05 -0700

    Updating documentation and functions

--- src/pyroxy/controllers/simple.py
+++ src/pyroxy/controllers/simple.py
@@ -31,8 +31,12 @@ def asbool(val):
     off of :func:`paste.deploy.converters.asbool` with a few stylistic tweaks
     added.
 
-    Raises a :exc:`ValueError` if the specified value cannot be coerced to a
-    True or False value.
+    :raises:
+        :exc:`ValueError` if the value could not be coerced to a :const:`True`
+        or :const:`False` value.
+    :returns:
+        :const:`True` or :const:`False`, depending on what :data:`val` was
+        coerced to.
     """
     # If its not a string or string-like object, just do our best.
     if not isinstance(val, basestring):
@@ -115,10 +119,11 @@ def filter_index(index_path):
     :param index_path:
         Absolute path to the index file.  This MUST already be created, and must
         be a file.
+    :raises:
+        If the file could not be opened, a 404 exception will be raised.
     :returns:
         An lxml formatted HTML page as a string, containing the final, filtered
-        version of the page.  If the file could not be opened, a 404 exception
-        will be raised.
+        version of the page.
     """
     try:
         fd = open(index_path, "r")
@@ -131,22 +136,22 @@ def filter_index(index_path):
     return lxml.html.tostring(html_tree)
 
 
-def remove_links(html_tree):
+def split_links(elements):
     """
-    Filters out links based on the various predicates.  Unfortunately, right
-    now, the predicates aren't configurable.
+    Parses through a list of elements, and splits them up based on their type.
 
-    :param html_tree:
-        :mod:`lxml.tree` representing the simple page.
-
-    Returns a modified :mod:`lxml.tree` with specific links filtered out.
+    :param iterable elements:
+        iterable containing :class:`lxml.etree.ElementTree.Element`
+    :returns:
+        Tuple of lists, ``(external_download_links, home_pages,
+        internal_download_links, unknown_links)``.
     """
     external_download_links = []
     home_pages = []
     internal_download_links = []
     unknown_links = []
 
-    for element in html_tree.iterfind(".//a"):
+    for element in elements:
         href = element.get("href")
         title = element.text_content()
 
@@ -157,26 +162,61 @@ def remove_links(html_tree):
         elif pred_filter_external_download_links(href, title):
             external_download_links.append(element)
         else:
-            log.debug("Filtering out link: %s (%s)", title, href)
             unknown_links.append(element)
 
+    return (external_download_links, home_pages, internal_download_links,
+            unknown_links)
+
+
+def build_links_to_remove(external_download_links, home_pages,
+        internal_download_links, unknown_links):
+    """
+    Generates a list of links to remove based on which links are preferred.
+
+    :param list external_download_links:
+        list of internal download links
+    :param list home_pages:
+        list of home pages
+    :param list internal_download_links:
+        list of internal download links
+    :param list unknown_links:
+        list of unknown links, or links that do not match either any of the
+        previous three.
+    :returns:
+        list of :class:`lxml.etree.ElementTree.Element` that are safe to be
+        removed.
+    """
     if internal_download_links:
-        log.debug("Internal download links used.")
-        if asbool(config.get('always_include_external', True)):
-            to_be_removed = home_pages + unknown_links
-        else:
-            to_be_removed = external_download_links + home_pages + unknown_links
+        to_be_removed = external_download_links + home_pages + unknown_links
     elif external_download_links:
-        log.debug("External download links used.")
         to_be_removed = home_pages + unknown_links
     elif home_pages:
         to_be_removed = unknown_links
     else:
-        log.debug("Home pages used.")
         to_be_removed = []
 
+    return to_be_removed
+
+
+def remove_links(html_tree):
+    """
+    Filters out links based on the various predicates.  Unfortunately, right
+    now, the predicates aren't configurable.
+
+    :param html_tree:
+        :mod:`lxml.tree` representing the simple page.
+    :returns:
+        A modified :mod:`lxml.tree` with specific links filtered out.
+    """
+    (external_download_links, home_pages, internal_download_links,
+            unknown_links) = split_links(html_tree.iterfind(".//a"))
+    to_be_removed = build_links_to_remove(external_download_links, home_pages,
+        internal_download_links, unknown_links)
+
+    # So that we preserve the funky formatting of the page, lets make sure that
+    # we just pull out the tags in place, without disrupting the HTML page
+    # itself.
     for element in to_be_removed:
-        log.debug("Filtering %s", title)
         element.getnext().drop_tree()
         element.drop_tree()
 
@@ -189,7 +229,7 @@ def package_list(package_name):
     """
     Controller used to serve up a package listing.
 
-    :param package_name:
+    :param string package_name:
         Name of the Python package to retrieve and filter the simple page for.
         Capitalization matters, depending on your operating system--i.e., on
         Linux, `pylons` != `Pylons`.
